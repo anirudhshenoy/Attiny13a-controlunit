268c85875769f409a30ac429b44bd37892283556
anirudhshenoy <anirudhshenoy92@gmail.com>
2018-10-23 19:48:08 +0530

    Fixed minor bug in Toggle PWM

--- main.c
+++ main.c
@@ -34,8 +34,9 @@ ISR(TIM0_COMPA_vect) {
 		}
 	}
 	else {																			//Toggle Mode
-		if (++timer_overflow_count > count) PORTB |= (1 << DDB4);
-		else PORTB &= ~(1 << DDB4);										//Software PWM Algo	
+		if (++timer_overflow_count < count) PORTB |= (1 << DDB4);
+		else PORTB &= ~(1 << DDB4);													//Software PWM Algo	
+		if (timer_overflow_count>256) timer_overflow_count=0;
 	}
 }
 
@@ -50,8 +51,10 @@ int main(void)
 	ADMUX = 0b00100001;				//PORTB |= 1<<PB4;
 	ADCSRA = 0b10000111;			//Enable ADC, div 16
 	TCCR0A |= (1<<WGM01);			// CTC Mode
-	OCR0A=234;						//Set top, compare match ~0.05secs
-    TIMSK0 |=1<<OCIE0A;				// enable timer overflow interrupt
+    TIMSK0 = 0b00000100;				// enable timer overflow interrupt
+	
+	TCCR0A=0x00;
+	TIMSK0=0b00000010;
     sei();
     while (1) {
 		if (trigger==1) {
@@ -63,48 +66,53 @@ int main(void)
 				timer_overflow_count = 0;
 				PORTB|=(1<<DDB4);									//Set O/P to high
 				count =count>>2;									//Calculate pulse high time 
-				trigger=0;				
+				trigger=0;
+				OCR0A=234;				
 				TCCR0B |= (1<<CS02);								// Pre-scale timer to 1/256th the clock rate	
-				
 			}
-			else{													// Toggle Mode
-				toggle^=0b1;
+			else if((PINB & 0b00001000)==0){						//Pulse Mode
+				PORTB|=(1<<DDB4);
+				OCR0A=234;
 				trigger=0;
 			}
-			
+			else {													// Toggle Mode
+				toggle^=0b1;
+				timer_overflow_count=0;								//Start Timer with no pre-scaler
+				OCR0A=1;
+				trigger=0;
+			}			
 		}
-		else {
-			if(((PINB & 0b00001000)==0) && (PINB & 0b00000010)){		//Pulse Mode and I/P is high
-				ADCSRA |= 0b01000000;
-				while (!(ADCSRA & (1 << ADIF)));
-				ADCSRA |= (1 << ADIF);
-				count = ADCH;
-				timer_overflow_count = 0;
-				count =count>>3;											//Calculate pulse duty cycle
-				trigger = 0; 
-				TCCR0B |= (1<<CS02);										// Pre-scale timer to 1/256th the clock rate
+		if(((PINB & 0b00001000)==0) && (PINB & 0b00000010)){		//Pulse Mode and I/P is high
+			ADCSRA |= 0b01000000;
+			while (!(ADCSRA & (1 << ADIF)));
+			ADCSRA |= (1 << ADIF);
+			count = ADCH;
+			count =count>>3;											//Calculate pulse duty cycle
+			TCCR0B |= (1<<CS02);										// Pre-scale timer to 1/256th the clock rate
 				
-			}
-			else if (!(PINB & 0b00001000)){							//Pulse Mode but button off
-				TCCR0B=0x00;
-				PORTB &= ~(1<<DDB4);
-			}
-			
 		}
-		if ((PINB & 0b00001000) && (PINB & 0b00000001)){			//Toggle Mode
+		else if ((PINB & 0b00001000) && (PINB & 0b00000001)){			//Toggle Mode
 			if(toggle){										// Toggle variable O/P is high (420 everyday)
 				ADCSRA |= 0b01000000;
 				while (!(ADCSRA & (1 << ADIF)));
 				ADCSRA |= (1 << ADIF);
 				count = ADCH;
-				timer_overflow_count=0;
-				TCCR0B|= (1<<CS00);							//Start Timer with no pre-scaler
+				TCCR0B|= (1<<CS00);
+				
 			}
 			else{
 				TCCR0B=0x00;
 				PORTB &= ~(1<<DDB4);
 			}
 		}
+		else if (!(PINB & 0b00001000)){							//Pulse Mode but button off
+			TCCR0B=0x00;
+			timer_overflow_count = 0;
+			PORTB &= ~(1<<DDB4);
+		}
+			
+		
+
 	}
     return 0;
 }
